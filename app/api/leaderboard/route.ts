9a6475f8cad69af3c52768ae18c0285a6b60c492
Brondy <bcfalsetto@gmail.com>
2026-05-04 22:53:15 -0500

    awrgweg

--- app/api/leaderboard/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getLeaderboard } from "@/lib/store";
-
-export async function GET() {
-  const board = await getLeaderboard();
-  return NextResponse.json(board);
-}
